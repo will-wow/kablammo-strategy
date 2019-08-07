48520c736fa0c62bac05ae7752f7c5a01c5049c6
Will Ockelmann-Wagner <wow@carbonfive.com>
2019-08-07 13:19:26 -0700

    print and fire

--- strategy.rb
+++ strategy.rb
@@ -4,10 +4,9 @@ require './examples/defensive'
 extend Aggressive
 extend Defensive
 
-@chooser = Proc.new do
-  rand() <= 0.5 ? act_aggressively : act_defensively
-end
 
 on_turn do
-  @chooser.call
+  p me
+  p battle
+  'f'
 end
